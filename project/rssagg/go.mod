374825074c0fd40b265b42019b7d0a02522e64ce
Dovydas Gulbinas <dovydas.gulbinas@protonmail.com>
2023-09-26 09:35:50 +0300

    initial commit

---
+++ project/rssagg/go.mod
@@ -0,0 +1,3 @@
+module github.com/dovydasgulbinas/rssagg
+
+go 1.21.0
---
+++ project/rssagg/main.go
@@ -0,0 +1,8 @@
+package main
+
+import ("fmt", "log", "os")
+
+func main() {
+	fmt.Println("hello world")
+  portString := os.Getenv("PORT")
+}
